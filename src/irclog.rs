43fca23ea1ed42a0b4e21085a29e2d5f0cc2b0a2
Thomas Preston <thomas.preston@codethink.co.uk>
2020-06-07 13:41:04 +0100

    irclog: Simplify the log searching and printing

--- src/irclog.rs
+++ src/irclog.rs
@@ -1,6 +1,7 @@
 //! Functions and data structures for parsing IRC standup logs
 
 use crate::StandupError;
+use std::fmt;
 use std::str::FromStr;
 
 #[derive(Debug)]
@@ -8,6 +9,7 @@ struct IrcLogLine {
     datetime: String,
     username: String,
     content: String,
+    in_discussion: bool,
 }
 
 impl FromStr for IrcLogLine {
@@ -19,7 +21,20 @@ impl FromStr for IrcLogLine {
             datetime: split.next().unwrap_or("").to_string(),
             username: split.next().unwrap_or("").to_string(),
             content: split.next().unwrap_or("").to_string(),
+            in_discussion: false,
         })
+    }
+}
+
+impl fmt::Display for IrcLogLine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.in_discussion {
+            write!(f, "    {}\t{}", self.username, self.content)
+        } else if self.content.starts_with('#') {
+            write!(f, "\n{}", self.content)
+        } else {
+            write!(f, "{}", self.content)
+        }
     }
 }
 
@@ -33,7 +48,7 @@ pub fn print_last_standup(
     // Read all of the lines, rposition requires std::iter::ExactSizeIterator
     let lines: Vec<&str> = irc_log.lines().collect();
 
-    // Mark the standup pattern locations, starting from the end of the log.
+    // Mark the standup pattern locations, starting from the end of the log
     let lstart = lines
         .iter()
         .rposition(|l| l.contains(start))
@@ -56,18 +71,17 @@ pub fn print_last_standup(
         ));
     }
 
-    for (i, l) in irc_log.lines().enumerate() {
-        if i >= lstart && i <= lend {
-            let line: IrcLogLine = l.parse()?;
-            if i <= ldiscussion {
-                if line.content.starts_with('#') {
-                    println!();
-                }
-                println!("{}", line.content);
-            } else {
-                println!("    {}\t{}", line.username, line.content);
-            }
-        }
+    // Parse the irc_log_lines
+    let mut irc_log_lines: Vec<IrcLogLine> = Vec::new();
+    for (i, l) in lines.iter().enumerate().skip(lstart).take(lend - lstart) {
+        let mut line: IrcLogLine = l.parse()?;
+        line.in_discussion = i > ldiscussion;
+        irc_log_lines.push(line);
+    }
+
+    // Print the irc_log_lines
+    for line in irc_log_lines {
+        println!("{}", line)
     }
 
     Ok(())
