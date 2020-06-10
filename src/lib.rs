3d4769cc05cecf13002447461f6884774a2ea99b
Thomas Preston <thomas.preston@codethink.co.uk>
2020-06-10 20:30:31 +0100

    lib: Add WIP tests

--- src/lib.rs
+++ src/lib.rs
@@ -134,15 +134,122 @@ mod test {
 
     #[test]
     fn test_irc_log_line() {
-        // TODO how do I get tabs which test in here?
-        let text = "2020-06-09 16:56:38     tpreston    foobar";
-        // TODO lose this unwrap
+        let text = "2020-06-09 16:56:38	tpreston	foobar";
         let irc_log_line: IrcLogLine = text.parse().unwrap();
-
         assert_eq!(irc_log_line.datetime, "2020-06-09 16:56:38");
         assert_eq!(irc_log_line.username, "tpreston");
         assert_eq!(irc_log_line.content, "foobar");
-        assert_eq!(irc_log_line.in_discussion, true);
+        assert_eq!(irc_log_line.in_discussion, false);
+    }
+
+    #[test]
+    fn test_rpos_str() {
+        let haystack = ["foo", "bar", "baz", "bar"];
+        assert_eq!(rpos_str(&haystack, "foo").ok(), Some(0));
+        assert_eq!(rpos_str(&haystack, "bar").ok(), Some(3));
+        assert_eq!(rpos_str(&haystack, "baz").ok(), Some(2));
+        assert!(rpos_str(&haystack, "bong").is_err());
+    }
+
+    #[test]
+    fn test_write_last_standup() {
+        let irc_log = "\
+2020-06-10 10:32:39	john-a	blar blah blar
+2020-06-10 10:33:52	john-b	:)
+2020-06-10 10:34:21	tpreston	great
+2020-06-10 10:34:22	tpreston	standup time!
+2020-06-10 10:34:24	tpreston	who is attending?
+2020-06-10 10:34:27	john-a	o/
+2020-06-10 10:34:28	john-b	o/
+2020-06-10 10:35:17	tpreston	order is me, john-a, john-b 
+2020-06-10 10:35:29	tpreston	## Thomas Preston (tpreston)
+2020-06-10 10:35:29	tpreston	* Done:
+2020-06-10 10:35:29	tpreston	  - something
+2020-06-10 10:35:29	tpreston	* Doing:
+2020-06-10 10:35:29	tpreston	  - something else
+2020-06-10 10:35:29	tpreston	* Next:
+2020-06-10 10:35:29	tpreston	  - another thing
+2020-06-10 10:35:29	tpreston	* Backlog:
+2020-06-10 10:35:29	tpreston	  - some thing I'll get around to
+2020-06-10 10:35:29	tpreston	## John Ayy (john-a)
+2020-06-10 10:35:55	john-a	* Done
+2020-06-10 10:35:55	john-a	  - A thing
+2020-06-10 10:35:55	john-a	* Today
+2020-06-10 10:35:55	john-a	  - This other thing
+2020-06-10 10:35:58	john-a	## John Bee (john-b)
+2020-06-10 10:36:02	john-b	* Done
+2020-06-10 10:36:02	john-b	    - Created the thing
+2020-06-10 10:36:02	john-b	    - Built another thing
+2020-06-10 10:36:02	john-b	    - Reviewed thingy
+2020-06-10 10:36:02	john-b	* Doing
+2020-06-10 10:36:04	john-b	    - Add some notes
+2020-06-10 10:36:06	john-b	    - Do a doo-hickey
+2020-06-10 10:36:08	john-b	    - Create the merge foo
+2020-06-10 10:36:10	john-b	# Discussion
+2020-06-10 10:36:25	 *	john-a doesn't have much concrete to be working on
+2020-06-10 10:36:41	john-a	Other than that, _o_
+2020-06-10 10:37:52	john-b	_o_
+2020-06-10 10:38:34	tpreston	if no points, closing standup in 5
+2020-06-10 10:38:35	tpreston	4
+2020-06-10 10:38:35	tpreston	3
+2020-06-10 10:38:36	tpreston	2
+2020-06-10 10:38:36	tpreston	1
+2020-06-10 10:38:38	tpreston	standup ends
+2020-06-10 10:38:40	tpreston	thanks all
+2020-06-10 10:38:53	john-b	ta tpreston";
+
+        let expected_standup = "\n## Thomas Preston (tpreston)
+* Done:
+  - something
+* Doing:
+  - something else
+* Next:
+  - another thing
+* Backlog:
+  - some thing I'll get around to
+
+## John Ayy (john-a)
+* Done
+  - A thing
+* Today
+  - This other thing
+
+## John Bee (john-b)
+* Done
+  - Created the thing
+  - Built another thing
+  - Reviewed thingy
+* Doing
+  - Add some notes
+  - Do a doo-hickey
+  - Create the merge foo
+
+# Discussion
+    *	john-a doesn't have much concrete to be working on
+    john-a	Other than that, _o_
+    john-b	_o_
+    tpreston	if no points, closing standup in 5
+    tpreston	4
+    tpreston	3
+    tpreston	2
+    tpreston	1
+";
+
+        let start = "## Thomas Preston (tpreston)";
+        let discussion = "# Discussion";
+        let end = "standup ends";
+        //let mut out_buffer = io::Cursor::new(vec![0, expected_standup.len()]);
+        let mut out_buffer = io::Cursor::new(vec![0; expected_standup.len()]);
+
+        let res = write_last_standup(&irc_log, start, discussion, end, &mut out_buffer);
+        assert!(res.is_ok());
+
+        let written_standup = out_buffer.get_ref();
+        println!(
+            "{:#?}",
+            &written_standup[..].iter().zip(expected_standup.as_bytes())
+        );
+        assert_eq!(expected_standup.as_bytes(), &written_standup[..]);
     }
 
     #[test]
