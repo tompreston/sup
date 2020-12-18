7cb482e1deca36439bcc0fd0002704095df4f1d2
Thomas Preston <thomas.preston@codethink.co.uk>
2020-12-18 11:27:12 +0000

    supf: Use --me and --next, which are more natural
    I couldn't get used to the other args. The new args -m and -n feel a bit
    more natural.

--- scripts/supf.py
+++ scripts/supf.py
@@ -7,7 +7,7 @@ import jinja2
 import yaml
 
 STANDUP_FILE = "/home/tpreston/w/standup.yaml"
-HEADER = {"name": "Thomas Preston", "username": "tpreston"}
+ME = {"name": "Thomas Preston", "username": "tpreston"}
 
 def in_lower(a, b):
     return a.lower() in b.lower()
@@ -32,10 +32,10 @@ def pr_standup(args):
         if s['project'] == args.project:
             standup = s
 
-    if args.name_header:
-        standup["name_header"] = HEADER
-    if args.tail:
-        standup["name_tail"] = get_member(args.tail, standup["members"])
+    if args.me:
+        standup["me"] = ME
+    if args.next:
+        standup["next"] = get_member(args.next, standup["members"])
 
     tloader = jinja2.FileSystemLoader("templates")
     tenv = jinja2.Environment(loader=tloader, lstrip_blocks=True)
@@ -46,9 +46,9 @@ def get_args():
     """Returns program arguments"""
     parser = argparse.ArgumentParser(description="sup prototype")
     parser.add_argument("project", help="Project Code")
-    parser.add_argument("-n", "--name-header", help="Print the header",
+    parser.add_argument("-m", "--me", help="Print standup ",
                         action="store_true")
-    parser.add_argument("-t", "--tail", help="Tail search term")
+    parser.add_argument("-n", "--next", help="Next person")
     return parser.parse_args()
 
 if __name__ == "__main__":
